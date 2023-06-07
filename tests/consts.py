0a7b1487ffcc58cc793b63d0d60877c5d545ee2f
kawanabe2 <kawanabe@penguins.co.jp>
2023-06-07 13:40:28 +0900

    Add test for multiple input files

--- tests/consts.py
+++ tests/consts.py
@@ -3,15 +3,21 @@ import os.path as path
 BASE_DIR = path.join(path.dirname(path.normpath(__file__)), "test_files" + path.sep)
 OUTPUT_DIR = BASE_DIR + "output" + path.sep
 
+# Turtle test files
 TTL_1 = BASE_DIR + "test_ttl_1.ttl"
-TTL_1_GZ = BASE_DIR + "test_ttl_1.ttl.gz"
 TTL_2 = BASE_DIR + "test_ttl_2.ttl"
 TTL_3 = BASE_DIR + "test_ttl_3.ttl"
+TTL_1_GZ = BASE_DIR + "test_ttl_1.ttl.gz"
+TTL_2_GZ = BASE_DIR + "test_ttl_2.ttl.gz"
+TTL_3_GZ = BASE_DIR + "test_ttl_3.ttl.gz"
 
+# N-Triples test files
 NT_1 = BASE_DIR + "test_nt_1.nt"
-NT_1_GZ = BASE_DIR + "test_nt_1.nt.gz"
 NT_2 = BASE_DIR + "test_nt_2.nt"
 NT_3 = BASE_DIR + "test_nt_3.nt"
+NT_1_GZ = BASE_DIR + "test_nt_1.nt.gz"
+NT_2_GZ = BASE_DIR + "test_nt_2.nt.gz"
+NT_3_GZ = BASE_DIR + "test_nt_3.nt.gz"
 
 OUTPUT_NT_1_GZ_MD = OUTPUT_DIR + "test_nt_1_gz.md"
 OUTPUT_NT_3_MD = OUTPUT_DIR + "test_nt_3.md"
---
+++ tests/test_get_input_prefixes_from_multi_files.py
@@ -0,0 +1,83 @@
+import unittest
+from doctor.doctor import get_input_prefixes_from_multi_files
+from tests.consts import TTL_1, TTL_2, TTL_3, TTL_1_GZ, TTL_2_GZ, TTL_3_GZ, NT_1, NT_2, NT_3, NT_1_GZ, NT_2_GZ, NT_3_GZ
+from shexer.consts import GZ
+
+class TestGetInputPrefixes(unittest.TestCase):
+
+    def test_nt(self):
+        input_prefixes = get_input_prefixes_from_multi_files([NT_1], None)
+        self.assertEqual(input_prefixes, [])
+
+    def test_ttl_1(self):
+        input_prefixes = get_input_prefixes_from_multi_files([TTL_1], None)
+        self.assertEqual(input_prefixes, [["rdf:", "http://www.w3.org/1999/02/22-rdf-syntax-ns#"], \
+                                            ["rdfs:", "http://www.w3.org/2000/01/rdf-schema#"], \
+                                            ["ex:", "http://example.org/"], \
+                                            ["foaf:", "http://xmlns.com/foaf/0.1/"], \
+                                            ["xsd:", "http://www.w3.org/2001/XMLSchema#"]])
+
+    def test_ttl_2(self):
+        input_prefixes = get_input_prefixes_from_multi_files([TTL_2], None)
+        self.assertEqual(input_prefixes, [["rdf:", "http://www.w3.org/1999/02/22-rdf-syntax-ns#"], \
+                                            ["ex:", "http://example.org#"], \
+                                            ["foaf:", "http://xmlns.com/foaf/0.1#"], \
+                                            ["xsd:", "http://www.w3.org/2001/XMLSchema#"]])
+
+    def test_ttl_3(self):
+        input_prefixes = get_input_prefixes_from_multi_files([TTL_3], None)
+        self.assertEqual(input_prefixes, [["rdf:", "http://www.w3.org/1999/02/22-rdf-syntax-ns#"], \
+                                            ["oboc:", "http://purl.obolibrary.org/obo/CHEBI_"], \
+                                            ["chebi:", "http://www.ebi.ac.uk/chebi/searchId.do?chebiId=CHEBI%3A"], \
+                                            ["xsd:", "http://www.w3.org/2001/XMLSchema#"], \
+                                            ["pobo:", "http://purl.obolibrary.org/obo/"]])
+
+    def test_nt_gz(self):
+        input_prefixes = get_input_prefixes_from_multi_files([NT_1_GZ], GZ)
+        self.assertEqual(input_prefixes, [])
+
+    def test_ttl_gz(self):
+        input_prefixes = get_input_prefixes_from_multi_files([TTL_1_GZ], GZ)
+        self.assertEqual(input_prefixes, [["rdf:", "http://www.w3.org/1999/02/22-rdf-syntax-ns#"], \
+                                            ["rdfs:", "http://www.w3.org/2000/01/rdf-schema#"], \
+                                            ["ex:", "http://example.org/"], \
+                                            ["foaf:", "http://xmlns.com/foaf/0.1/"], \
+                                            ["xsd:", "http://www.w3.org/2001/XMLSchema#"]])
+
+
+    def test_nt_multi(self):
+        input_prefixes = get_input_prefixes_from_multi_files([NT_1, NT_2, NT_3], None)
+        self.assertEqual(input_prefixes, [])
+
+    def test_ttl_multi(self):
+        input_prefixes = get_input_prefixes_from_multi_files([TTL_1, TTL_2, TTL_3], None)
+        self.assertEqual(input_prefixes, [["rdf:", "http://www.w3.org/1999/02/22-rdf-syntax-ns#"], \
+                                            ["rdfs:", "http://www.w3.org/2000/01/rdf-schema#"], \
+                                            ["ex:", "http://example.org/"], \
+                                            ["foaf:", "http://xmlns.com/foaf/0.1/"], \
+                                            ["xsd:", "http://www.w3.org/2001/XMLSchema#"], \
+                                            ["ex:", "http://example.org#"], \
+                                            ["foaf:", "http://xmlns.com/foaf/0.1#"], \
+                                            ["oboc:", "http://purl.obolibrary.org/obo/CHEBI_"], \
+                                            ["chebi:", "http://www.ebi.ac.uk/chebi/searchId.do?chebiId=CHEBI%3A"], \
+                                            ["pobo:", "http://purl.obolibrary.org/obo/"]])
+
+    def test_ttl_gz_multi(self):
+        input_prefixes = get_input_prefixes_from_multi_files([TTL_1_GZ, TTL_2_GZ, TTL_3_GZ], GZ)
+        self.assertEqual(input_prefixes, [["rdf:", "http://www.w3.org/1999/02/22-rdf-syntax-ns#"], \
+                                            ["rdfs:", "http://www.w3.org/2000/01/rdf-schema#"], \
+                                            ["ex:", "http://example.org/"], \
+                                            ["foaf:", "http://xmlns.com/foaf/0.1/"], \
+                                            ["xsd:", "http://www.w3.org/2001/XMLSchema#"], \
+                                            ["ex:", "http://example.org#"], \
+                                            ["foaf:", "http://xmlns.com/foaf/0.1#"], \
+                                            ["oboc:", "http://purl.obolibrary.org/obo/CHEBI_"], \
+                                            ["chebi:", "http://www.ebi.ac.uk/chebi/searchId.do?chebiId=CHEBI%3A"], \
+                                            ["pobo:", "http://purl.obolibrary.org/obo/"]])
+
+    def test_nt_gz_multi(self):
+        input_prefixes = get_input_prefixes_from_multi_files([NT_1_GZ, NT_3_GZ, NT_3_GZ], GZ)
+        self.assertEqual(input_prefixes, [])
+
+if __name__ == "__main__":
+    unittest.main()
